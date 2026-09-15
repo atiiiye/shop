037bce095ff7167a8594ddea28456b7b8174a89c
atiii <aty.sokhansanj@gmail.com>
2026-09-16 01:17:01 +0330

    feat: orders app & shopping cart

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 
+from orders.forms import CartAddForm
 from products.models import Product, Category
 from . import tasks
 
@@ -19,7 +20,8 @@ class HomeView(View):
 class PorductDetailsViews(View):
     def get(self, request, slug):
         product = get_object_or_404(Product, slug=slug)
-        return render(request, 'home/detail.html', {"product": product})
+        form = CartAddForm()
+        return render(request, 'home/detail.html', {"product": product, "form": form})
 
 
 class BucketListView(View):
