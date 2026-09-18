89f25830823414d320ff50a023a108dfb5523bdd
atiii <aty.sokhansanj@gmail.com>
2026-09-19 01:19:10 +0330

    feat: set context processors

--- orders/cart.py
+++ orders/cart.py
@@ -25,6 +25,9 @@ class Cart:
             yield item
 
 
+    def __len__(self):
+        return sum( item['quantity'] for item in self.cart.values())
+
     def add(self, product, quantity):
         product_id = str(product.id)
         if product_id not in self.cart:
